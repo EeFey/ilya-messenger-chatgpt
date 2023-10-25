4e5efb7fe9acecae31f75b7ecc50715b63eac320
eefey <16509521+EeFey@users.noreply.github.com>
2023-10-25 23:37:12 +0800

    add chatgpt request retry

--- src/services/ChatGPT.ts
+++ src/services/ChatGPT.ts
@@ -4,9 +4,12 @@ import { OPENAI_API_KEY, CHATGPT_MODEL, CHATGPT_MAX_TOKENS, CHATGPT_TEMPERATURE 
 
 import { Message } from '../interfaces/Message';
 import { AvailableGPTFunctions, GPTFunctionDefinition } from '../interfaces/GPTFunctions';
+import { delay } from '../utils/Utils';
 
 export class ChatGPT {
   private readonly openai: typeof OpenAIApi;
+  private static readonly MAX_RETRIES = 5;
+  private static readonly INITIAL_RETRY_DELAY = 2000;
 
   constructor(
     private readonly availableFunctions: AvailableGPTFunctions,
@@ -18,7 +21,60 @@ export class ChatGPT {
     this.openai = new OpenAIApi(configuration);
   }
 
-  private async createChatCompletion(
+  public async getReply(
+    roleDescription: string,
+    message?: string | null,
+    messageQueue?: Message[] | null,
+    useFunctions: boolean = false
+  ) {
+    let messages: Message[] = [{ role: "system", content: roleDescription }];
+
+    if (messageQueue) messages = messages.concat(messageQueue);
+    if (message) messages.push({ role: "user", content: message });
+
+    const completion =  await this.createChatCompletionRequest(messages, useFunctions);
+
+    const responseMessage = completion.data.choices[0].message;
+    if (responseMessage.function_call) {
+      const functionName = responseMessage.function_call.name as keyof AvailableGPTFunctions;
+      const functionToCall = this.availableFunctions[functionName];
+      const functionArgs = JSON.parse(responseMessage.function_call.arguments);
+      const functionResponse = await functionToCall(functionArgs.query);
+
+      if (!functionResponse && responseMessage.content) return responseMessage.content;
+
+      messages.splice(1, messageQueue ? messageQueue.length : 0);
+      messages.push(responseMessage);
+      messages.push({ role: "function", name: functionName, content: functionResponse });
+      const secondResponse = await this.createChatCompletionRequest(messages, false);
+
+      return secondResponse.data.choices[0].message.content;
+    }
+
+    return responseMessage.content;
+  }
+
+  private async createChatCompletionRequest(
+    messages: Message[],
+    useFunctions: boolean = false
+  ) {
+    const request = this.buildChatCompletionRequest(messages, useFunctions);
+    for (let i = 0; i < ChatGPT.MAX_RETRIES; i++) {
+      try {
+        return await this.openai.createChatCompletion(request);
+      } catch (error) {
+        if (this.isRateLimitError(error)) {
+          console.log('Rate limit hit. Retrying...');
+          await delay(ChatGPT.INITIAL_RETRY_DELAY * (i + 1));
+        } else {
+          throw error;
+        }
+      }
+    }
+    throw new Error('Rate limit exceeded. Please try again later.');
+  }
+
+  private buildChatCompletionRequest(
     messages: Message[],
     useFunctions: boolean = false
   ) {
@@ -34,39 +90,11 @@ export class ChatGPT {
       request.function_call = "auto";
     }
 
-    return await this.openai.createChatCompletion(request);
+    return request;
   }
 
-  public async getReply(
-    roleDescription: string,
-    message?: string | null,
-    messageQueue?: Message[] | null,
-    useFunctions: boolean = false
-  ) {
-    let messages: Message[] = [{ role: "system", content: roleDescription }];
-
-    if (messageQueue) messages = messages.concat(messageQueue);
-    if (message) messages.push({ role: "user", content: message });
-
-    const completion =  await this.createChatCompletion(messages, useFunctions);
-
-    const responseMessage = completion.data.choices[0].message;
-    if (responseMessage.function_call) {
-      const functionName = responseMessage.function_call.name as keyof AvailableGPTFunctions;
-      const functionToCall = this.availableFunctions[functionName];
-      const functionArgs = JSON.parse(responseMessage.function_call.arguments);
-      const functionResponse = await functionToCall(functionArgs.query);
-
-      if (!functionResponse && responseMessage.content) return responseMessage.content;
-
-      messages.splice(1, messageQueue ? messageQueue.length : 0);
-      messages.push(responseMessage);
-      messages.push({ role: "function", name: functionName, content: functionResponse });
-      const secondResponse = await this.createChatCompletion(messages, false);
-
-      return secondResponse.data.choices[0].message.content;
-    }
-
-    return responseMessage.content;
+  private isRateLimitError(error: any): boolean {
+    return error.response && error.response.status === 429;
   }
+
 }
